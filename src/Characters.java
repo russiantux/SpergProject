972a0a76cf5cf2e3e4ea689e1f0be2a9b1542470
Viktor <vguy.lazarev@gmail.com>
2017-05-16 13:45:56 -0700

    added a basic foundation to the character class

--- src/Characters.java
+++ src/Characters.java
@@ -1,4 +1,43 @@
 
 public class Characters {
+	
+	private String charName = "";
+	private int HealthNum = 0;
+	private int ManaNum = 0;
+	private String[] CharList = new String [5];
+	private int[] HealthList = new int [5];
+	private int[] ManaList = new int[5];
+	
+	
+	public Characters(){
+		String name = charName;
+		int health = HealthNum;
+		int mana = ManaNum;
+		
+	}
+	
+	private void addChar(String name, int health, int mana){
+		int arrNum = 1;
+		int ListNum = 0;
+		
+		if(arrNum > 6)
+		{
+			CharList[ListNum] = name;
+			HealthList[ListNum] = health;
+			ManaList[ListNum] = mana;
+			ListNum++;
+			arrNum++;
+			
+		}
+		
+	}
+	private String listStats(int arrayNum){
+		return CharList[arrayNum] + HealthList[arrayNum] + ManaList[arrayNum];
+	}
+	
+	public void WrittingtoFile() {
+		
+		
+	}
 
 }
