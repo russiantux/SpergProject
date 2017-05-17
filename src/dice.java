7f7babd564c2ceb226da3c27c2319ee5f3a04ee4
Technomancer-ALX <alxst335@gmail.com>
2017-05-17 12:11:35 -0700

    created instances of dice class (d2, d4, d6, d10, d20, d100)
    Cyka Blyat this is hard...

--- src/dice.java
+++ src/dice.java
@@ -2,11 +2,11 @@
 public class dice {
 	//
 	private int dNumSides;
-	private int dNum;
+	private int dNum = 1;
 
 	
 	public dice(int numSides, int numD) {
-		//Lets the number of sides on dice and the number of dice vary
+		//Lets the number of sides on dice and the number of dice vary. Default is 1 die.
 		numSides = dNumSides;
 		numD = dNum;
 		}
@@ -15,6 +15,11 @@ public class dice {
 	}
 	
 	dice d2 = new dice(2);
+	dice d4 = new dice(4);
+	dice d6 = new dice(6);
+	dice d10 = new dice(10);
+	dice d20 = new dice(20);
+	dice d100 = new dice(100);
 	
 	public int roll(int rollNum){
 		
