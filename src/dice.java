6bbdcf49a62a01a92472ef5dee1b079a61055eed
Viktor <vguy.lazarev@gmail.com>
2017-05-19 10:51:14 -0700

    matching dice file to dice branch so no conflicts when merging

--- src/dice.java
+++ src/dice.java
@@ -1,16 +1,24 @@
-
 public class dice {
 	//
 	private int dNumSides;
-	private int dNum;
+	private int dNum = 1;
 
 	
 	public dice(int numSides, int numD) {
-		 numSides = dNumSides;
-		 numD = dNum;
-		
-		//work in progress
+		//Lets the number of sides on dice and the number of dice vary. Default is 1 die.
+		numSides = dNumSides;
+		numD = dNum;
+		}
+	public dice(int numSides){
+		numSides = dNumSides;
 	}
+	
+	dice d2 = new dice(2);
+	dice d4 = new dice(4);
+	dice d6 = new dice(6);
+	dice d10 = new dice(10);
+	dice d20 = new dice(20);
+	dice d100 = new dice(100);
 	
 	public int roll(int rollNum){
 		
