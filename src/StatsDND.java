774d430d9207693bbfd43807d32127d163163e67
Viktor <vguy.lazarev@gmail.com>
2017-05-19 11:52:10 -0700

    finally fixed the dice and char to work

--- src/StatsDND.java
+++ src/StatsDND.java
@@ -7,12 +7,24 @@ public class StatsDND  {
 	}
 	public static void main(String[] args) {      
 		Characters test1 = new Characters();
+		
 
 		test1.addChar("testname", 100, 10);
 		
-		System.out.print(test1.listStats(1));
+		System.out.print(test1.listStats(0));
+		System.out.println("     ");
+		System.out.println("d20 value: " + dice.roll(20));
+		System.out.println("     ");
+		System.out.println("d20 value: " + dice.roll(2));
+		System.out.println("     ");
+		System.out.println("d20 value: " + dice.roll(10));
+		System.out.println("     ");
+		System.out.println("d20 value: " + dice.roll(6));
+		
+	
 		
 	}
 
 
 }
+	
